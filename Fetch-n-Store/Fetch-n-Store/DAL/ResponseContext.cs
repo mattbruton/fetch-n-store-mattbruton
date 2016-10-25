44ea583d1b916bed97dec0fbd4f4b21aca774f8e
MATT BRUTON <the.bruton@gmail.com>
2016-10-24 20:41:09 -0500

    completed ReponseContext

--- Fetch-n-Store/Fetch-n-Store/DAL/ResponseContext.cs
+++ Fetch-n-Store/Fetch-n-Store/DAL/ResponseContext.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using Fetch_n_Store.Models;
 
 namespace Fetch_n_Store.DAL
 {
-    public class ResponseContext
+    public class ResponseContext : DbContext
     {
+        public virtual DbSet<Response> Responses { get; set; }
     }
 }
