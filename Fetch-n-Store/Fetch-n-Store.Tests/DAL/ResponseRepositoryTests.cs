888158348bb7bbe714c181b7e68745e753b12973
MATT BRUTON <the.bruton@gmail.com>
2016-10-24 20:29:34 -0500

    created reponse context, repository files, created res repo unit test file

---
+++ Fetch-n-Store/Fetch-n-Store.Tests/DAL/ResponseRepositoryTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fetch_n_Store.Tests.DAL
+{
+    [TestClass]
+    public class ResponseRepositoryTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Fetch-n-Store/Fetch-n-Store/DAL/ResponseContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fetch_n_Store.DAL
+{
+    public class ResponseContext
+    {
+    }
+}
