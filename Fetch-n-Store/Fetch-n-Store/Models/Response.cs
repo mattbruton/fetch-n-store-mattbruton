f95a85a33667e91bef109173026a473bacf97309
MATT BRUTON <the.bruton@gmail.com>
2016-10-24 20:26:53 -0500

    created erd, model for response

---
+++ Fetch-n-Store/Fetch-n-Store/Models/Response.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fetch_n_Store.Models
+{
+    public class Response
+    {
+        [Key]
+        public int ResponseId { get; set; }
+        public string URL { get; set; }
+        public string StatusCode { get; set; }
+        public string HttpMethod { get; set; }
+        public string ResponseTime { get; set; }
+    }
+}
